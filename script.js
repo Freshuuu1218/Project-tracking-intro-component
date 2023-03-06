561971206050381ca4cd7521650fc6d5c470b0ea
Kacper Klimczak <kacper_klimczak_2609@protonmail.com>
2023-03-06 20:29:54 +0100

    fix menu display

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const nav = document.querySelector('nav');
 const menuBtn = document.querySelector('#menu');
 
+function displayMenu(){
+        if(window.innerWidth > 768){
+        menuBtn.style.display = 'none';
+        nav.style.display = 'flex';
+    }else{
+        nav.style.display = 'none';
+        menuBtn.style.display = 'flex';
+    }
+}
+displayMenu()
+
 menuBtn.addEventListener('click',()=>{
     if(menuBtn.getAttribute('src') ==='./images/icon-hamburger.svg'){
         menuBtn.setAttribute('src','./images/icon-close.svg');
@@ -10,13 +21,6 @@ menuBtn.addEventListener('click',()=>{
         nav.style.display = 'none';
     }
 })
-
 window.addEventListener('resize',()=>{
-    if(window.innerWidth > 768){
-        menuBtn.style.display = 'none';
-        nav.style.display = 'flex';
-    }else{
-        menuBtn.style.display = 'flex';
-        nav.style.display = 'none';
-    }
+    displayMenu()
 })
